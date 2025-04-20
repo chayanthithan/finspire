0597ccc33d991e2ce91bfad285057212d46f432b
cchayanthithan <kumarchayan31@gmail.com>
2025-04-20 16:22:49 +0530

    import components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "../reusable-components/header/header.component";
-import { FooterComponent } from "../reusable-components/footer/footer.component";
-import { HomeComponent } from "../pages/home/home.component";
-import { ContactComponent } from "../pages/contact/contact.component";
-import { ServiceComponent } from "../pages/service/service.component";
-import { AboutComponent } from "../pages/about/about.component";
-import { ProjectsComponent } from "../pages/projects/projects.component";
+import { HeaderComponent } from './reusable-components/header/header.component';
+import { FooterComponent } from './reusable-components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from '../pages/about/about.component';
-import { ContactComponent } from '../pages/contact/contact.component';
-import { ServiceComponent } from '../pages/service/service.component';
-import { ProjectsComponent } from '../pages/projects/projects.component';
-import { HomeComponent } from '../pages/home/home.component';
+
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
